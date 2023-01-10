59e8e915e8df3d51a4f0278283307d8479fec966
ElisabethNyssens <babettenyssens@gmail.com>
2023-01-10 17:54:38 +0100

    cleaning

--- src/main/java/com/spring/henallux/Innocent/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/spring/henallux/Innocent/configuration/WebSecurityConfiguration.java
@@ -15,9 +15,7 @@ import org.springframework.security.web.authentication.SavedRequestAwareAuthenti
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private static final String LOGIN_REQUEST = "/login";
-/*    private static final String LOGIN_FAILURE_REQUEST = LOGIN_REQUEST + "/error";*/
     private static final String[] AUTHORIZED_REQUESTS_ANYBODY = new String[]{"/","/welcome","/register","/register/**","/register/send","/products/**","/detail/**","/about","/css/**","/images/**"};
-   /* private static final String[] AUTHORIZED_REQUESTS_ADMIN = new String[]{"/admin"};*/
 
     private UserDetailsService userDetailsServiceImpl;
 
@@ -32,25 +30,24 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable(); // Disable because not necessary
+        http.csrf().disable();
 
         http
-                .authorizeRequests() // We define the authorization here
-               /* .antMatchers(AUTHORIZED_REQUESTS_ADMIN).hasRole("ADMIN")*/ // For the request to "/admin", the user needs to be an admin
-                .antMatchers(AUTHORIZED_REQUESTS_ANYBODY).permitAll() // For the request to the index page, any user has access
-                .anyRequest().authenticated() // For all the other requests, the user needs to be authenticated
+                .authorizeRequests()
+                .antMatchers(AUTHORIZED_REQUESTS_ANYBODY).permitAll()
+                .anyRequest().authenticated()
 
                 .and()
-                .formLogin() // We define the login part here.
-                .successHandler(new SavedRequestAwareAuthenticationSuccessHandler()) // provided by spring to redirect to the last request
-                .loginPage(LOGIN_REQUEST) // We specify a login page. Otherwise spring creates one by default
-                .permitAll() // To make the login page the available for any user
+                .formLogin()
+                .successHandler(new SavedRequestAwareAuthenticationSuccessHandler())
+                .loginPage(LOGIN_REQUEST)
+                .permitAll()
 
                 .and()
-                .logout() // We define the logout part here - By default : URL = "/logout"
-                .logoutUrl("/innocent/logout") // If other link than "/logout" (that is by default)
-                .logoutSuccessUrl("/login")  // URL to return if logout is successfull
-                .permitAll(); // To make the logout available for any user
+                .logout()
+                .logoutUrl("/innocent/logout")
+                .logoutSuccessUrl("/login")
+                .permitAll();
     }
 
     /**
